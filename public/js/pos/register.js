97f62cc710aa990c7aebfb04b68594be26f31adf
ncip48 <mbahcip00@gmail.com>
2026-06-20 21:39:51 +0700

    fix: propprtion image

--- public/js/pos/register.js
+++ public/js/pos/register.js
@@ -180,7 +180,7 @@ $(function () {
                         `
                     : `
                             <div class="flex items-center justify-center w-full h-full text-slate-300">
-                                <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <svg class="w-30 h-30" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
                                         d="M4 16l4-4a3 3 0 014.243 0L20 20M14 14l1-1a3 3 0 014.243 0L20 14M4 8h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                 </svg>
